a2df1d4b6c9aa1a42ad3f32eccc2ae19ee89bf41
RANA-USAMA <ranausamauet3430@gmail.com>
2021-10-27 15:06:31 +0500

    warning

--- app/screens/OtpScreens.js
+++ app/screens/OtpScreens.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { RFPercentage } from 'react-native-responsive-fontsize';
@@ -11,26 +11,44 @@ import Screen from './../components/Screen';
 import Colors from '../config/Colors';
 
 function OtpScreens(props) {
+
+    const [fullText, setFullText] = useState(false);
+
     return (
         <Screen style={{ flex: 1, justifyContent: 'flex-start', alignItems: "center", backgroundColor: Colors.white }}>
+
             {/* Back Icon */}
             <TouchableOpacity style={{ position: 'absolute', left: RFPercentage(4), top: RFPercentage(7) }}>
                 <Ionicons name="chevron-back" style={{ fontSize: RFPercentage(3.5) }} color={Colors.primary} />
             </TouchableOpacity>
 
-            {/* Heading */}
-            <Text style={{ marginTop: RFPercentage(22), fontSize: RFPercentage(3.5), color: Colors.primary, fontWeight: 'bold' }}>
-                Enter Pin
-            </Text>
+            <View style={{ marginTop: RFPercentage(20), justifyContent: 'center', alignItems: 'center' }}>
+
+                {fullText ? <Text style={{ color: "#ff0000", fontSize: RFPercentage(2.5) }}>
+                    Pin is incorrect
+                </Text> : null}
+
+                <Text style={{ marginTop: RFPercentage(7), fontSize: RFPercentage(3.5), color: Colors.primary, fontWeight: 'bold' }}>
+                    Enter Pin
+                </Text>
+            </View>
 
             {/* Otp Input */}
             <OTPInputView
                 style={{ width: '80%', height: RFPercentage(20) }}
                 pinCount={6}
+                onCodeFilled={() => setFullText(true)}
                 secureTextEntry={true}
                 placeholderTextColor={"black"}
                 autoFocusOnLoad
-                codeInputFieldStyle={styles.underlineStyleBase}
+                codeInputFieldStyle={{
+                    width: 30,
+                    height: 45,
+                    borderWidth: 0,
+                    borderBottomWidth: 1,
+                    fontSize: RFPercentage(3.5),
+                    color: fullText ? "#ff0000" : Colors.primary
+                }}
                 codeInputHighlightStyle={styles.underlineStyleHighLighted}
             />
         </Screen>
@@ -45,15 +63,6 @@ const styles = StyleSheet.create({
 
     borderStyleHighLighted: {
         borderColor: "#03DAC6",
-    },
-
-    underlineStyleBase: {
-        width: 30,
-        height: 45,
-        borderWidth: 0,
-        borderBottomWidth: 1,
-        fontSize: RFPercentage(3.5),
-        color: Colors.primary
     },
 
     underlineStyleHighLighted: {
