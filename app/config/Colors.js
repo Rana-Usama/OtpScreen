7aca3a89ee88f2649f7189d921ffc3cba7d97765
salman2481 <sallu warraich 2481>
2021-10-27 12:56:06 +0500

    otp done

--- app/config/Colors.js
+++ app/config/Colors.js
@@ -2,9 +2,10 @@ export default {
     primary: '#493fba',
     grey: "#708097",
     inputFieldBorder: "rgba(195, 211, 212, 0.9)",
-    inputFieldBackgroundColor: "#E2E8F0",
+    inputFieldBackgroundColor: "rgba(242, 242, 242, 0.4)",
     inputFieldPlaceholder: "#9AA3AE",
     white: "#fff",
     black: "black",
     lightBlue: '#C0FAEB',
+    warn: "#ba4848"
 };
--- app/screens/OtpScreens.js
+++ app/screens/OtpScreens.js
@@ -1,50 +1,40 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { RFPercentage } from 'react-native-responsive-fontsize';
 
 //components
 import Screen from './../components/Screen';
-import InputField from './../components/InputField';
 
 //config
 import Colors from '../config/Colors';
 
 function OtpScreens(props) {
+    const ref_input2 = useRef();
+    const ref_input3 = useRef();
+    const ref_input4 = useRef();
+    const ref_input5 = useRef();
+    const ref_input6 = useRef();
 
-    const [inputField, SetInputField] = useState([
-        {
-            placeholder: "1",
-            value: "",
-        },
-        {
-            placeholder: "2",
-            value: "",
-        },
-        {
-            placeholder: "3",
-            value: "",
-        },
-        {
-            placeholder: "4",
-            value: "",
-        },
-        {
-            placeholder: "5",
-            value: "",
-        },
-        {
-            placeholder: "6",
-            value: "",
-        },
-    ]);
+    const [value1, setValue1] = useState('')
+    const [value2, setValue2] = useState('')
+    const [value3, setValue3] = useState('')
+    const [value4, setValue4] = useState('')
+    const [value5, setValue5] = useState('')
+    const [value6, setValue6] = useState('')
 
-    const handleChange = (text, i) => {
-        let tempfeilds = [...inputField];
-        tempfeilds[i].value = text;
-        SetInputField(tempfeilds);
+    const [warn, showWarn] = useState(false)
 
-    };
+    const handleLastValue = (text) => {
+        setValue6(text)
+
+        if (value1 && value2 && value3 && value4 && value5 && text) {
+            showWarn(true)
+            console.log("dshf")
+        } else {
+            showWarn(false)
+        }
+    }
 
     return (
 
@@ -54,33 +44,110 @@ function OtpScreens(props) {
                 <Ionicons name="chevron-back" style={{ fontSize: RFPercentage(3.5) }} color={Colors.primary} />
             </TouchableOpacity>
 
-            <View style={{ marginTop: RFPercentage(20), justifyContent: 'center', alignItems: 'center' }}>
-                {/* {fullText ? <Text style={{ color: "#ff0000", fontSize: RFPercentage(2.5) }}>
-                    Pin is incorrect
-                </Text> : null} */}
-                <Text style={{ marginTop: RFPercentage(7), fontSize: RFPercentage(3.5), color: Colors.primary, fontWeight: 'bold' }}>
-                    Enter Pin
+            <View style={{ marginTop: RFPercentage(5), justifyContent: 'center', alignItems: 'center' }}>
+                {
+                    warn ?
+                        <Text style={{ marginTop: RFPercentage(7), fontSize: RFPercentage(2.5), color: Colors.warn }}>
+                            PINs do not match
+                        </Text> : null
+                }
+            </View>
+
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ marginTop: warn ? RFPercentage(7) : RFPercentage(16), fontSize: RFPercentage(3.5), color: Colors.primary, fontWeight: 'bold' }}>
+                    {warn ? "Confirm New PIN" : "Enter Old PIN"}
                 </Text>
             </View>
-            <View style={{ flexDirection: 'row' }}>
-                {inputField.map((item, i) => (
-                    <View key={i} style={{ marginTop: RFPercentage(2) }} >
-                        <InputField
-                            backgroundColor={Colors.inputFieldBackgroundColor}
-                            borderColor={"#E3E5E5"}
-                            borderWidth={0}
-                            borderRadius={RFPercentage(1)}
-                            fontSize={RFPercentage(2.3)}
-                            handleFeild={(text) => handleChange(text, i)}
-                            value={item.value}
-                            width={"30%"}
-                        />
-                    </View>
-                ))}
+            <View style={{ flexDirection: 'row', marginTop: RFPercentage(2) }}>
+                <View style={{ marginTop: RFPercentage(2), marginRight: RFPercentage(1.3), alignItems: "center", justifyContent: "center", flexDirection: "row", backgroundColor: Colors.inputFieldBackgroundColor, width: RFPercentage(6), height: RFPercentage(6.9), marginVertical: RFPercentage(0.7), }} >
+                    <TextInput
+                        placeholderTextColor={Colors.primary}
+                        onChangeText={(text) => setValue1(text)}
+                        value={value1.length === 1 ? "*" : ""}
+                        maxLength={1}
+                        keyboardType="numeric"
+                        textAlign="center"
+                        style={{ color: warn ? Colors.warn : Colors.primary, alignSelf: "center", fontSize: RFPercentage(3), width: "90%" }}
+                        autoFocus={true}
+                        returnKeyType="next"
+                        onSubmitEditing={() => ref_input2.current.focus()}
+                    />
+                </View>
+
+                <View style={{ marginTop: RFPercentage(2), marginRight: RFPercentage(1.3), alignItems: "center", justifyContent: "center", flexDirection: "row", backgroundColor: Colors.inputFieldBackgroundColor, width: RFPercentage(6), height: RFPercentage(6.9), marginVertical: RFPercentage(0.7), }} >
+                    <TextInput
+                        placeholderTextColor={Colors.primary}
+                        onChangeText={(text) => setValue2(text)}
+                        value={value2.length === 1 ? "*" : ""}
+                        maxLength={1}
+                        keyboardType="numeric"
+                        textAlign="center"
+                        style={{ color: warn ? Colors.warn : Colors.primary, alignSelf: "center", fontSize: RFPercentage(3), width: "90%" }}
+                        returnKeyType="next"
+                        onSubmitEditing={() => ref_input3.current.focus()}
+                        ref={ref_input2}
+                    />
+                </View>
+
+                <View style={{ marginTop: RFPercentage(2), marginRight: RFPercentage(1.3), alignItems: "center", justifyContent: "center", flexDirection: "row", backgroundColor: Colors.inputFieldBackgroundColor, width: RFPercentage(6), height: RFPercentage(6.9), marginVertical: RFPercentage(0.7), }} >
+                    <TextInput
+                        placeholderTextColor={Colors.primary}
+                        onChangeText={(text) => setValue3(text)}
+                        value={value3.length === 1 ? "*" : ""}
+                        maxLength={1}
+                        keyboardType="numeric"
+                        textAlign="center"
+                        style={{ color: warn ? Colors.warn : Colors.primary, alignSelf: "center", fontSize: RFPercentage(3), width: "90%" }}
+                        returnKeyType="next"
+                        onSubmitEditing={() => ref_input4.current.focus()}
+                        ref={ref_input3}
+                    />
+                </View>
+
+                <View style={{ marginTop: RFPercentage(2), marginRight: RFPercentage(1.3), alignItems: "center", justifyContent: "center", flexDirection: "row", backgroundColor: Colors.inputFieldBackgroundColor, width: RFPercentage(6), height: RFPercentage(6.9), marginVertical: RFPercentage(0.7), }} >
+                    <TextInput
+                        placeholderTextColor={Colors.primary}
+                        onChangeText={(text) => setValue4(text)}
+                        value={value4.length === 1 ? "*" : ""}
+                        maxLength={1}
+                        keyboardType="numeric"
+                        textAlign="center"
+                        style={{ color: warn ? Colors.warn : Colors.primary, alignSelf: "center", fontSize: RFPercentage(3), width: "90%" }}
+                        returnKeyType="next"
+                        onSubmitEditing={() => ref_input5.current.focus()}
+                        ref={ref_input4}
+                    />
+                </View>
+
+                <View style={{ marginTop: RFPercentage(2), marginRight: RFPercentage(1.3), alignItems: "center", justifyContent: "center", flexDirection: "row", backgroundColor: Colors.inputFieldBackgroundColor, width: RFPercentage(6), height: RFPercentage(6.9), marginVertical: RFPercentage(0.7), }} >
+                    <TextInput
+                        placeholderTextColor={Colors.primary}
+                        onChangeText={(text) => setValue5(text)}
+                        value={value5.length === 1 ? "*" : ""}
+                        maxLength={1}
+                        keyboardType="numeric"
+                        textAlign="center"
+                        style={{ color: warn ? Colors.warn : Colors.primary, alignSelf: "center", fontSize: RFPercentage(3), width: "90%" }}
+                        returnKeyType="next"
+                        onSubmitEditing={() => ref_input6.current.focus()}
+                        ref={ref_input5}
+                    />
+                </View>
+
+                <View style={{ marginTop: RFPercentage(2), marginRight: RFPercentage(1.3), alignItems: "center", justifyContent: "center", flexDirection: "row", backgroundColor: Colors.inputFieldBackgroundColor, width: RFPercentage(6), height: RFPercentage(6.9), marginVertical: RFPercentage(0.7), }} >
+                    <TextInput
+                        placeholderTextColor={Colors.primary}
+                        onChangeText={(text) => handleLastValue(text)}
+                        value={value6.length === 1 ? "*" : ""}
+                        maxLength={1}
+                        keyboardType="numeric"
+                        textAlign="center"
+                        style={{ color: warn ? Colors.warn : Colors.primary, alignSelf: "center", fontSize: RFPercentage(3), width: "90%" }}
+                        ref={ref_input6}
+                    />
+                </View>
 
             </View>
-
-
         </Screen>
     );
 }
