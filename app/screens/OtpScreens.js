cfe2196224585e788ec23ed5a8b58ad04094d751
RANA-USAMA <ranausamauet3430@gmail.com>
2021-10-27 14:41:09 +0500

    almost done

--- app/screens/OtpScreens.js
+++ app/screens/OtpScreens.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { RFPercentage } from 'react-native-responsive-fontsize';
+import OTPInputView from '@twotalltotems/react-native-otp-input'
 
 //components
 import Screen from './../components/Screen';
@@ -17,11 +18,48 @@ function OtpScreens(props) {
                 <Ionicons name="chevron-back" style={{ fontSize: RFPercentage(3.5) }} color={Colors.primary} />
             </TouchableOpacity>
 
-            <Text style={{ marginTop: RFPercentage(22), fontSize: RFPercentage(2.8), color: Colors.primary, fontWeight: 'bold' }}>
+            {/* Heading */}
+            <Text style={{ marginTop: RFPercentage(22), fontSize: RFPercentage(3.5), color: Colors.primary, fontWeight: 'bold' }}>
                 Enter Pin
             </Text>
+
+            {/* Otp Input */}
+            <OTPInputView
+                style={{ width: '80%', height: RFPercentage(20) }}
+                pinCount={6}
+                secureTextEntry={true}
+                placeholderTextColor={"black"}
+                autoFocusOnLoad
+                codeInputFieldStyle={styles.underlineStyleBase}
+                codeInputHighlightStyle={styles.underlineStyleHighLighted}
+            />
         </Screen>
     );
 }
+
+const styles = StyleSheet.create({
+    borderStyleBase: {
+        width: 30,
+        height: 45
+    },
+
+    borderStyleHighLighted: {
+        borderColor: "#03DAC6",
+    },
+
+    underlineStyleBase: {
+        width: 30,
+        height: 45,
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        fontSize: RFPercentage(3.5),
+        color: Colors.primary
+    },
+
+    underlineStyleHighLighted: {
+        borderColor: Colors.primary,
+    },
+
+});
 
 export default OtpScreens;
